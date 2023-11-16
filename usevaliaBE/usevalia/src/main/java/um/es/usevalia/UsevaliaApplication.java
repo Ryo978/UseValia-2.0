b965b08e4625f6a481a0c676182378e4c657f57f
José Ayllón <jose.ayllonbenitez@gmail.com>
2023-11-16 19:21:32 +0100

    Setup BE & FE

---
+++ usevaliaBE/usevalia/src/main/java/um/es/usevalia/UsevaliaApplication.java
@@ -0,0 +1,13 @@
+package um.es.usevalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsevaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsevaliaApplication.class, args);
+	}
+
+}
---
+++ usevaliaBE/usevalia/src/test/java/um/es/usevalia/UsevaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package um.es.usevalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsevaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
